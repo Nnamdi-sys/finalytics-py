b82db520d2fc8f110b788be8b4ce52e4cb461772
Nnamdi Olisaeloka <57339313+Nnamdi-sys@users.noreply.github.com>
2024-01-13 19:18:02 +0800

    finalytics-0.2.2

--- conf.py
+++ conf.py
@@ -24,6 +24,5 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'basic'
 html_static_path = ['_static']
-html_theme_options = {'body_max_width': '80%'}
